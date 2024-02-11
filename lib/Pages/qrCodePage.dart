eee019ada39da16694c96a41e455d81c5394c77e
AGENT1736 <khaliddiab2003@gmail.com>
2024-02-11 20:07:48 +0200

    added functioning qr code

--- lib/Pages/qrCodePage.dart
+++ lib/Pages/qrCodePage.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class QrCodePage extends StatefulWidget {
   const QrCodePage({super.key});
@@ -9,6 +11,26 @@ class QrCodePage extends StatefulWidget {
 
 
 class _QrCodePageState extends State<QrCodePage> {
+
+  final qrKey = GlobalKey(debugLabel: 'QR');
+
+  Barcode? barcode;
+
+  QRViewController? controller;
+
+  @override
+  void dispose(){
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  void reassemble() async{
+    if (Platform.isAndroid){
+      await controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +59,49 @@ class _QrCodePageState extends State<QrCodePage> {
           ),
         ),
       ),
+      body: Stack(alignment: Alignment.center,
+      children:<Widget> [
+        buildQrView(context),
+        Positioned(
+          bottom: 10,
+            child: buildResult()
+        )
+      ],
+      )
     );
   }
+
+  Widget buildQrView(BuildContext context) => QRView(
+      key: qrKey,
+      onQRViewCreated: onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+        cutOutSize: MediaQuery.of(context).size.width * 0.8,
+        borderWidth: 10,
+        borderLength: 20,
+        borderRadius: 10,
+        borderColor: Colors.blueAccent
+      ),
+  );
+
+  void onQRViewCreated(QRViewController controller)
+  {
+    setState(() => this.controller = controller);
+
+    controller.scannedDataStream.listen((barcode)=> setState(() {
+      this.barcode = barcode;
+    }));
+  }
+
+  Widget buildResult () => Container(
+    padding: const EdgeInsets.all(12),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(8),
+      color: Colors.white24,
+    ),
+    child: Text(
+      barcode != null ? "Result: ${barcode!.code}" : "Scan a code!",
+      maxLines: 3,),
+  );
 }
+
+
