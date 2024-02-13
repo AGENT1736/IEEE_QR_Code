e4225d5b3aed8d7396d059d4ec9d6a880801b6dc
AGENT1736 <khaliddiab2003@gmail.com>
2024-02-13 20:35:10 +0200

    added a new login page removed non functional login page

---
+++ lib/Pages/LoginPage.dart
@@ -0,0 +1,158 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:ieee_qr_code/Pages/qrCodePage.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:toggle_switch/toggle_switch.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+
+  TextEditingController emailController = new TextEditingController();
+  TextEditingController usernameController = new TextEditingController();
+  TextEditingController passwordController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        leading: const Icon(
+          Icons.person,
+          color: Colors.white,
+        ),
+        title: const Center(
+          child: Text("Login",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white
+            ),
+          ),
+        ),
+      ),
+
+
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center ,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+
+            //developer exclusive
+
+
+            //email input
+            const Text("Email",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20
+              )
+              ,),
+
+
+            SizedBox(height: 10,width:screenWidth),
+
+            //Email Text form field
+            SizedBox(
+              width: 250,
+              child: TextFormField(
+                controller: emailController,
+                decoration:const InputDecoration(
+                  border: OutlineInputBorder(borderRadius:BorderRadius.all(
+                      Radius.circular(25)
+                  ),
+                      borderSide:BorderSide(color: Colors.black
+                          ,width: 2.0
+                      )
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20,),
+
+
+            //password
+            const Text("Password",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20
+                )
+            ),
+            const SizedBox(height: 10),
+
+
+            //password text form field
+            SizedBox(
+              width: 250,
+              child: TextFormField(
+                controller: passwordController,
+                obscureText: true,
+                decoration:const InputDecoration(
+                  border: OutlineInputBorder(borderRadius:BorderRadius.all(
+                      Radius.circular(25)
+                  ),
+                      borderSide:BorderSide(color: Colors.black
+                          ,width: 2.0
+                      )
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20,),
+
+
+            SizedBox(
+              width: 250,
+              child: AnimatedButton(
+                text: "LOGIN",
+                color: Colors.blue[900],
+                pressEvent: (){
+                  AwesomeDialog(
+                    context: context,
+                    dialogType: DialogType.question,
+                    title: "CONFIRM?",
+                    showCloseIcon: true,
+                    animType: AnimType.scale,
+                    btnOkText: "YES!",
+                    btnCancelText: "NO!",
+                    btnOkOnPress: (){
+                      setState(() async{
+                        try {
+                          final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+                            email: emailController.text,
+                            password: passwordController.text,
+                          );
+
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => const QrCodePage()),);
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'weak-password') {
+                            print('The password provided is too weak.');
+                          } else if (e.code == 'email-already-in-use') {
+                            print('The account already exists for that email.');
+                          }
+                        } catch (e) {
+                          print(e);
+                        }
+                      });
+                    }
+                  ).show();
+                } ,
+              ),
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
