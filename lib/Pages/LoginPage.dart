202d8d64c863dcf5275e1536f614d99b863acea1
AGENT1736 <khaliddiab2003@gmail.com>
2024-02-13 22:59:39 +0200

    idk what the change is for tbh XD

--- lib/Pages/LoginPage.dart
+++ lib/Pages/LoginPage.dart
@@ -18,7 +18,8 @@ class _LoginPageState extends State<LoginPage> {
 
 
   TextEditingController emailController = new TextEditingController();
-  TextEditingController usernameController = new TextEditingController();
+  //
+  // TextEditingController usernameController = new TextEditingController();
   TextEditingController passwordController = new TextEditingController();
 
   @override
